8649bed300e1cc76a8ae6664898aa184cdc00610
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-09-21 09:37:51 -0300

    adicionado sétimo requisito do projeto

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -35,10 +35,23 @@
 */
 
 const calculator = (number1, number2) => {
+    const objeto = {
+      sum: number1 + number2,
+      mult: number1 * number2,
+      div: Math.floor(number1 / number2),
+      sub: number1 - number2,
+    };
+    return objeto;
 };
 
 const arrayGenerator = (type, object) => {
-
+  if (type === 'keys') {
+    return Object.keys(object);
+  }
+  if (type === 'values') {
+    return Object.values(object);
+  }
+  return Object.entries(object);
 };
 
 module.exports = { calculator, arrayGenerator };
